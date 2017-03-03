9a2b6fa4ab27d1d05fdd66a34bc561d2fc375a10
Evan Shortiss <evanshortiss@gmail.com>
2017-03-03 12:32:59 -0800

    provide option to use mongodb

--- application.js
+++ application.js
@@ -5,7 +5,6 @@ const log = require('fh-bunyan').getLogger(__filename);
 const mbaasApi = require('fh-mbaas-api');
 const mbaasExpress = mbaasApi.mbaasExpress();
 const fhRestExpress = require('fh-rest-express-router');
-const fhRestMemoryAdapter = require('fh-rest-memory-adapter');
 const app = module.exports = express();
 
 // Note: the order which we add middleware to Express here is important!
@@ -21,7 +20,14 @@ app.use(
   '/users',
   fhRestExpress({
     name: 'users',
-    adapter: fhRestMemoryAdapter(),
+
+    // Store user data in process memory...
+    adapter: require('fh-rest-memory-adapter')(),
+
+    // ...or use a mongodb running locally on port 27017
+    // adapter: require('fh-rest-mongodb-adapter')({
+    //   collection: 'users'
+    // }),
     bodyAndQueryValidations: require('./validations/users/body-query'),
     routeParamValidations: require('./validations/users/route-params')
   })
--- validations/users/route-params.js
+++ validations/users/route-params.js
@@ -6,20 +6,30 @@ const Joi = require('joi');
 module.exports = {
   read: [{
     // Incoming GET (read) requests must contain a numeric ID param in the url
+    // or if using mongo an _id string
     schema: Joi.object().keys({
-      id: Joi.number()
+      id: Joi.alternatives(
+        Joi.number(),
+        Joi.string()
+      )
     })
   }],
   update: [{
     // Incoming PUT (update) requests must contain a numeric ID param in the url
     schema: Joi.object().keys({
-      id: Joi.number()
+      id: Joi.alternatives(
+        Joi.number(),
+        Joi.string()
+      )
     })
   }],
   delete: [{
     // Incoming DELETE (delete) requests must contain a numeric ID param in the url
     schema: Joi.object().keys({
-      id: Joi.number()
+      id: Joi.alternatives(
+        Joi.number(),
+        Joi.string()
+      )
     })
   }]
 };
